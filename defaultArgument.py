310419650463c3e150024726c119241e2cec29a3
Saurav Shah <thesauravs@Sauravs-MacBook-Pro.local>
2019-09-02 19:34:27 +0530

    Default values of Argument

---
+++ defaultArgument.py
@@ -0,0 +1,10 @@
+def get_gender(sex = 'Unkown'):
+    if sex is 'm':
+        sex = 'Male'
+    elif sex is 'f':
+        sex = 'Female'
+    print(sex)
+
+get_gender('m')
+get_gender('f')
+get_gender()
